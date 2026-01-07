aef916ae4ecdcaad944d5363d71d1853e699064f
obvly <zazedu4@gmail.com>
2026-01-07 15:34:58 +0100

    Update

--- src/main/java/core/basesyntax/StorageImpl.java
+++ src/main/java/core/basesyntax/StorageImpl.java
@@ -2,30 +2,35 @@ package core.basesyntax;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS = 10;
-    private int size;
-    private V[] values;
     private K[] keys;
+    private V[] values;
+    private int size;
 
     public StorageImpl() {
         this.keys = (K[]) new Object[MAX_ITEMS];
         this.values = (V[]) new Object[MAX_ITEMS];
-        }
+        this.size = 0;
+    }
 
-        @Override
-        public void put(K key, V value) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
-                    values[i] = value;
-                    return;
-                }
-            }
+    @Override
+    public void put(K key, V value) {
+        int index = getIndex(key);
+
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
-            @Override
-        public V get(K key) {
-        for (int i = 0; i< size; i++) {
-            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
-                return values[i];
-            }
+    }
+
+    @Override
+    public V get(K key) {
+        int index = getIndex(key);
+
+        if (index != -1) {
+            return values[index];
         }
         return null;
     }
@@ -33,5 +38,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
